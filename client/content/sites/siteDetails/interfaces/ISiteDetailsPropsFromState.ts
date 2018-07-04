1f744d368b8db0dccbc503f234ec74cf70d1acd1
Hardik <hardik.mintyfusion@outlook.com>
2018-07-04 18:01:16 +0530

    sort  imports

--- client/content/sites/siteDetails/interfaces/ISiteDetailsPropsFromState.ts
+++ client/content/sites/siteDetails/interfaces/ISiteDetailsPropsFromState.ts
@@ -1,5 +1,5 @@
-import IRightPaneProps from "../../../common/rightPane/interfaces/IRightPaneProps";
 import IJobModel from "../../../../models/jobs/IJobModel";
+import IRightPaneProps from "../../../common/rightPane/interfaces/IRightPaneProps";
 import ISiteModel from "../../../../models/sites/ISiteModel";
 
 export default interface ISiteDetailsPropsFromState {
