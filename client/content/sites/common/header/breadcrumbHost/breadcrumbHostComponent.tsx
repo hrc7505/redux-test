6e0b6f61e4e650f4f7aef0388f528551931afb0e
Sherwin <szeminkong@gmail.com>
2018-07-10 14:09:52 -0700

    Created selector for memoization of breadcrumb props

--- client/content/sites/common/header/breadcrumbHost/breadcrumbHostComponent.tsx
+++ client/content/sites/common/header/breadcrumbHost/breadcrumbHostComponent.tsx
@@ -1,4 +1,4 @@
-import { Breadcrumb, IBreadcrumbItem } from "office-ui-fabric-react/lib/Breadcrumb";
+import { Breadcrumb } from "office-ui-fabric-react/lib/Breadcrumb";
 import * as React from "react";
 
 import IBreadcrumbHostProps from "content/sites/common/header/breadcrumbHost/interfaces/IBreadcrumbHostProps";
@@ -6,54 +6,11 @@ import IBreadcrumbHostProps from "content/sites/common/header/breadcrumbHost/int
 import "content/sites/common/header/breadcrumbHost/breadcrumbHostStyle.scss";
 
 export default class BreadcrumbHostComponent extends React.PureComponent<IBreadcrumbHostProps> {
-    constructor(props: IBreadcrumbHostProps) {
-        super(props);
-
-        this.items = [];
-        this.handleOperations(props.locationPath);
-    }
-
-    private items: IBreadcrumbItem[];
-
     public render(): JSX.Element {
         return (
             <div className="cPanel breadcrumbs">
-                <Breadcrumb items={this.items} />
+                <Breadcrumb items={this.props.breadcrumbItems} />
             </div >
         );
-    }
-
-    // TODO: Remove componentWillReceiveProps as it is deprecated and should not be used.
-    public componentWillReceiveProps(nextProps: IBreadcrumbHostProps): void {
-        if (this.props.locationPath !== nextProps.locationPath) {
-            this.handleOperations(nextProps.locationPath);
-        }
-    }
-
-    private handleClickForLink = (link: string): void => {
-        this.props.history.push({ pathname: link });
-    }
-
-    private handleOperations(locationPath: string): void {
-        if (locationPath) {
-            const pathList: string[] = locationPath.split("/");
-            pathList.splice(0, 1);
-
-            const items: IBreadcrumbItem[] = [];
-            let link: string = "/";
-
-            pathList.map((data: string, i: number) => {
-                link += link === "/" ? data : ("/" + data);
-                const newLink: string = link;
-
-                items.push({
-                    key: data,
-                    text: data.replace(/\b\w/g, (x: string) => x.toUpperCase()),
-                    onClick: (): void => this.handleClickForLink(newLink)
-                });
-            });
-
-            this.items = items;
-        }
     }
 }
--- client/content/sites/common/header/breadcrumbHost/breadcrumbHostContainer.ts
+++ client/content/sites/common/header/breadcrumbHost/breadcrumbHostContainer.ts
@@ -1,14 +1,63 @@
+import * as H from "history";
+import { IBreadcrumbItem } from "office-ui-fabric-react/lib/Breadcrumb";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
+import { createSelector, OutputParametricSelector } from "reselect";
 
 import BreadcrumbHostComponent from "content/sites/common/header/breadcrumbHost/breadcrumbHostComponent";
 import IBreadcrumbHostProps from "content/sites/common/header/breadcrumbHost/interfaces/IBreadcrumbHostProps";
 import IBreadcrumbPropsFromState from "content/sites/common/header/breadcrumbHost/interfaces/IBreadcrumbPropsFromState";
 import IAppState from "duck/interfaces/IAppState";
 
-function mapStateToProps(state: IAppState): IBreadcrumbPropsFromState {
+type GetBreadcrumbPathFromHeaderState = (state: IAppState) => string;
+type ComponentRouterHistory = (state: IAppState, props: IBreadcrumbHostProps) => H.History;
+type ResultFunction = (path: string, history: H.History) => IBreadcrumbItem[];
+
+const getBreadcrumbPath: GetBreadcrumbPathFromHeaderState =
+    (state: IAppState): string => state.sitesState.headerState.breadcrumbPath;
+const componentRouterHistory: ComponentRouterHistory =
+    (state: IAppState, props: IBreadcrumbHostProps): H.History => props.history;
+
+const computeBreadcrumbItems:
+    OutputParametricSelector<IAppState, IBreadcrumbHostProps, IBreadcrumbItem[], ResultFunction> = createSelector(
+        [getBreadcrumbPath, componentRouterHistory],
+        (path: string, history: H.History): IBreadcrumbItem[] => {
+            if (!path) {
+                // If no path was found in the state, return an emtpy array so that the breadcrumb renders nothing.
+                return [];
+            } else {
+                // Splitting the path by the slash character to obtain the parts of the path.
+                const pathList: string[] = path.split("/");
+                // Removing the first result since the path will always start with a slash resulting in an
+                // empty string first result.
+                pathList.splice(0, 1);
+
+                const items: IBreadcrumbItem[] = [];
+                let link: string = "/";
+
+                pathList.map((data: string, i: number) => {
+                    // Sequentially build the links for each of the breadcrumb entries to be used
+                    // for the onClick property.
+                    link += link === "/" ? data : ("/" + data);
+                    const newLink: string = link;
+
+                    // TODO: Once we have a way to obtain the names of the entities, we should insert them here
+                    //       in the text property of the correct breadcrumb items.
+                    items.push({
+                        key: data,
+                        text: data.replace(/\b\w/g, (x: string) => x.toUpperCase()),
+                        onClick: (): void => history.push(newLink),
+                    });
+                });
+
+                return items;
+            }
+        }
+);
+
+function mapStateToProps(state: IAppState, props: IBreadcrumbHostProps): IBreadcrumbPropsFromState {
     return {
-        locationPath: state.sitesState.headerState.breadcrumbPath
+        breadcrumbItems: computeBreadcrumbItems(state, props),
     };
 }
 
